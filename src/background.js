8289a51af84c5090b5578820f05951fcdf0b1a2f
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-05-12 20:55:21 +0100

    Add files via upload

---
+++ src/background.js
@@ -0,0 +1,13 @@
+//   alert('this is not working!!');
+//   let allAds = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower', 'reclama_right', 'reclama_articol_start', 'taboola-alternating-below-article', 'reclama_articol_final']; 
+//   let homepage = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower'];
+//   let arr = homepage.map((element) => {
+//     if (element) return document.getElementById(element);
+// });
+//   arr.map((element) => console.log(arr));
+
+console.log('test');
+
+  // let adsHomepage = ['reclama_homepage_middle', 'reclama_homepage_lower'];
+
+  
