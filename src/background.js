5dbed89c10b8e37018e75bbe31c950dd28c5771c
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-05-14 20:47:08 +0100

    changed permissions for manifest

--- src/background.js
+++ src/background.js
@@ -1,10 +1,9 @@
-//   alert('this is not working!!');
-//   let allAds = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower', 'reclama_right', 'reclama_articol_start', 'taboola-alternating-below-article', 'reclama_articol_final']; 
-//   let homepage = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower'];
-//   let arr = homepage.map((element) => {
-//     if (element) return document.getElementById(element);
-// });
-//   arr.map((element) => console.log(arr));
+  let allAds = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower', 'reclama_right', 'reclama_articol_start', 'taboola-alternating-below-article', 'reclama_articol_final']; 
+  let homepage = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower'];
+  let arr = homepage.map((element) => {
+    if (element) return document.getElementById(element);
+});
+  arr.map((element) => console.log(arr));
 
 console.log('test');
 
