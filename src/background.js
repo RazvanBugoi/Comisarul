5b50ac1dbfaf672760aaaf98df997d782b12b9eb
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-05-18 15:58:55 +0100

    trying to hide adds from articles

--- src/background.js
+++ src/background.js
@@ -1,12 +1,11 @@
-  let allAds = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower', 'reclama_right', 'reclama_articol_start', 'taboola-alternating-below-article', 'reclama_articol_final']; 
+  let articles = ['reclama_right', 'reclama_articol_start', 'taboola-alternating-below-article', 'reclama_articol_final'];
   let homepage = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower'];
-  let arr = homepage.map((element) => {
+  let home = homepage.map((element) => {
     if (element) return document.getElementById(element);
-});
-  arr.map((element) => console.log(arr));
+  });
+  home.map((element) => element.hidden = true);
 
-console.log('test');
-
-  // let adsHomepage = ['reclama_homepage_middle', 'reclama_homepage_lower'];
-
-  
+  let page = articles.map((element) => {
+    if (element) return document.getElementById(element);
+  });
+  articles.map((element) => element.hidden = true);
