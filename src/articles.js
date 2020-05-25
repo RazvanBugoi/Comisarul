d6ad13b4a08d07aaee47985fdfa369eaa098b506
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-05-25 11:32:54 +0100

    checking if the elements exist before removing

--- src/articles.js
+++ src/articles.js
@@ -1,7 +1,11 @@
-let articles = ['adocean_728', 'reclama_right', 'reclama_articol_start',
+let articles = ['adocean_728', 'reclama_right', 'reclama_articol_start', "vi-stories-top-div",
 'pa_1x1_psbk_1590090626319', 'taboola-alternating-below-article', 'reclama_articol_final'];
 
 let page = articles.map((element) => {
     if (element) return document.getElementById(element);
   });
-  page.map((element) => element.hidden = true);
+  page.map((element) => {
+    if(element) {
+      element.remove()
+    }
+  })
--- src/background.js
+++ src/background.js
@@ -1,13 +1,10 @@
-  // let articles = ['adocean_728', 'reclama_right', 'reclama_articol_start',
-  // 'taboola-alternating-below-article', 'reclama_articol_final'];
-  let homepage = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower'];
+let homepage = ['reclama_homepage_middle', 'adocean_728', 'reclama_homepage_lower'];
   
-  let home = homepage.map((element) => {
-    if (element) return document.getElementById(element);
-  });
-  home.map((element) => element.hidden = true);
-
-  // let page = articles.map((element) => {
-  //   if (element) return document.getElementById(element);
-  // });
-  // page.map((element) => element.hidden = true);
+let home = homepage.map((element) => {
+  if (element) return document.getElementById(element);
+});
+home.map((element) => {
+  if(element) {
+    element.remove()
+  }
+});
